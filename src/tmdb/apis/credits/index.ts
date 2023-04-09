0af6b1c439e586d01b423a5709b84142122e75be
Leandro <leandrowkz@gmail.com>
2023-04-09 22:05:01 +0100

    refactor: move credits api to new format

--- src/tmdb/apis/credits/index.ts
+++ src/tmdb/apis/credits/index.ts
@@ -1,12 +1,11 @@
-import { BaseAPI } from './BaseAPI'
+import { BaseAPI } from '../BaseAPI'
 import { TMDBResponse } from 'src/types/TMDBResponse'
-import { Credit } from 'src/types/Credit'
+import { Credit } from './types'
 
 export class Credits extends BaseAPI {
   /**
    * Get a movie or TV credit details by id.
    *
-   * @returns Promise<TMDBResponse<Credit>>
    * @see https://developers.themoviedb.org/3/credits/get-credit-details
    */
   public async details(creditId: string): Promise<TMDBResponse<Credit>> {
